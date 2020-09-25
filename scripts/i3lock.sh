63ded22e5ea42c3866b6073e6baa5128a5555348
buk0w5k1 <th3buk0w5k1@gmail.com>
2020-09-24 19:04:22 -0600

    Initial commit

---
+++ scripts/i3lock.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# i3lock script using i3lock-color
+exec i3lock -B sigma --insidecolor=00000000 --insidevercolor=00000000 --insidewrongcolor=00000000 --ringvercolor=e901e1 --ringwrongcolor=e901e1 --ringcolor=e901e1 --keyhlcolor=980193 --separatorcolor=660066 --veriftext="checking..." --noinputtext="clear" --wrongtext="incorrect" --greetercolor=e901e1 --radius=90 --verifcolor=e901e1 --wrongcolor=e901e1  --locktext="locked" --lockfailedtext="failed"
